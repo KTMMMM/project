e1457839f3b1bec3f0ec97e25c830b131582ab12
KTMMMM <110892552+KTMMMM@users.noreply.github.com>
2022-11-16 19:07:42 +0900

    tm

--- character/skincolor.py
+++ character/skincolor.py
@@ -5,15 +5,12 @@ import numpy as np
 
 
 def select_color(R,G,B):         
-    mR = np.mean(R)
-    mG = np.mean(G)
-    mB = np.mean(B)
 
-    color = np.array([mR,mG,mB])
-    preset1 = np.array([100.,200.,125.])
-    preset2 = np.array([121.,231.,32.])
-    preset3 = np.array([94.,77.,152.])
-    preset4 = np.array([251.,65.,25.])
+    color = np.array([R,G,B])
+    preset1 = np.array([147.,90.,78.])
+    preset2 = np.array([172.,136.,126.])
+    preset3 = np.array([172.,143.,122.])
+    preset4 = np.array([104.,63.,46.])
 
     color_norm = np.sqrt(np.sum(np.square(color)))
     preset1_norm = np.sqrt(np.sum(np.square(preset1)))
@@ -28,42 +25,45 @@ def select_color(R,G,B):
         'color4': np.sum(color*preset4)/(color_norm*preset4_norm)
 
     }
-    return max(similar_color)
+    print(similar_color)
+    return max(similar_color, key=similar_color.get)
 
 def skin_detect(img):
-    contour_list = [152,148,176,149,150,136,172,58,132,93,234,127,162,21,54,103,67,109,10,338,297,332,284,251,389,356,454,323,361,288,397,365,379,378,400,377]
-    point_list=[]
     B = []
     G = []
     R = []
-    
+
     mp_face_mesh = mp.solutions.face_mesh
     with mp_face_mesh.FaceMesh(static_image_mode=True,
                                 max_num_faces=1,
                                 refine_landmarks=True,
                                 min_detection_confidence=0.5) as face_mesh:
-        # image = cv2.imread(path)
+        # image = cv2.imread('test.jpg')
         image = img
                         # Convert the BGR image to RGB before processing.
         results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         # Print and draw face mesh landmarks on the image.
         if not results.multi_face_landmarks:
-            pass
-
+            print('Undetected')
+            return 'Undetected'
         mesh_dot = results.multi_face_landmarks[0].landmark
-        for i in contour_list:
-            point_x = int(image.shape[1]*mesh_dot[i].x)
-            point_y = int(image.shape[0]*mesh_dot[i].y)
-            point_list.append([point_x,point_y])
+        
+        point_x = int(image.shape[1]*mesh_dot[50].x)
+        point_y = int(image.shape[0]*mesh_dot[50].y)
+        point_x_2 = int(image.shape[1]*mesh_dot[280].x)
+        point_y_2 = int(image.shape[0]*mesh_dot[280].y)
+        
+        colors = image[point_x][point_y]
+        colors2 = image[point_x_2][point_y_2]
 
-            for i in point_list:
-                colors = image[i[0]][i[1]]
-                colors = colors.tolist()
-                B.append(colors[0])
-                G.append(colors[1])
-                R.append(colors[2])
-                
-    result = select_color(R,G,B)
-    # print(result)
+        if colors[2] >= colors2[2]:
+            B=colors[0]
+            G=colors[1]
+            R=colors[2]
+        else:
+            B=colors2[0]
+            G=colors2[1]
+            R=colors2[2]
+        result = select_color(R,G,B)
+        # print(result)
     return result
-
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, Form, Request
+from fastapi import FastAPI, File
 import mediapipe as mp
 import cv2
 from character.detect_shape import face_classifi
@@ -6,20 +6,37 @@ from character.skincolor import skin_detect
 import io
 import numpy as np
 from PIL import Image
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class Item(BaseModel):
+    id : str
+    time : str
+    x : str
+    y : str
+    
+class coorperates(BaseModel):
+    coorperate : Optional[List[Item]] = None
 
 app = FastAPI()
 
 @app.get("/")
-def read_rood():
+def hello():
     return{"Hello":"World"}
 
-@app.post("/files/")
-async def create_file(file: bytes= File(...)):
-    print("얼굴 데이터터")
+@app.post("/facedata")
+async def face_analysis(file: bytes= File(...)):
     image = np.array(Image.open(io.BytesIO(file)))
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
     fshape = face_classifi(image)
     sColor = skin_detect(image)
-    
+    print(sColor)
+    print("얼굴 데이터 완료")
     return {"face shape" : fshape,
             "skin color" : sColor}
+    
+@app.post("/user-analysis")
+async def user_correlation(item:coorperates):
+    print(item)
+    return item
